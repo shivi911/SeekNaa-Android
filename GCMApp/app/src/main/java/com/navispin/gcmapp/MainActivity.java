467bb0cacba035b1a6aa30b55256d726f7016578
shivi911 <shivi@Shivs-MacBook-Pro-3.local>
2016-09-18 20:55:38 -0500

    GCM for Android

---
+++ GCMApp/app/src/main/java/com/navispin/gcmapp/MainActivity.java
@@ -0,0 +1,61 @@
+package com.navispin.gcmapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
+    private boolean isReceiverRegistered;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String token = "";
+
+        sharedPreferences.getString(QuickstartPreferences.GCM_TOKEN, token);
+        Log.d("MYGCM", "TOKEN IS " + token);
+
+
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                SharedPreferences sharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(context);
+                boolean sentToken = sharedPreferences
+                        .getBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, false);
+
+            }
+        };
+
+        registerReceiver();
+
+
+        // Start IntentService to register this application with GCM.
+        Intent intent = new Intent(this, RegistrationIntentService.class);
+        startService(intent);
+
+
+    }
+
+    private void registerReceiver(){
+        if(!isReceiverRegistered) {
+            LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                    new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
+            isReceiverRegistered = true;
+        }
+    }
+}
---
+++ GCMApp/app/src/main/java/com/navispin/gcmapp/QuickstartPreferences.java
@@ -0,0 +1,10 @@
+package com.navispin.gcmapp;
+
+/**
+ * Created by shivi on 9/17/16.
+ */
+public class QuickstartPreferences {
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String GCM_TOKEN = "gcmToken";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+}
